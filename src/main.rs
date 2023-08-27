81f4be7eb6da5468fc46fc703e8344321a56dd58
Yaşar İdikut <yasar8000@gmail.com>
2023-08-27 13:55:06 -0400

    Set up the hello world for Vue + Actix

---
+++ src/main.rs
@@ -0,0 +1,39 @@
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use mime_guess::from_path;
+use rust_embed::RustEmbed;
+
+#[derive(RustEmbed)]
+#[folder = "./web/dist/"]
+struct Asset;
+
+fn handle_embedded_file(path: &str) -> HttpResponse {
+    match Asset::get(path) {
+        Some(content) => HttpResponse::Ok()
+            .content_type(from_path(path).first_or_octet_stream().as_ref())
+            .body(content.data.into_owned()),
+        None => HttpResponse::NotFound().body("404 Not Found"),
+    }
+}
+
+#[actix_web::get("/")]
+async fn web_ui_index() -> impl Responder {
+    handle_embedded_file("index.html")
+}
+
+#[actix_web::get("/{_:.*}")]
+async fn web_ui_dist(path: web::Path<String>) -> impl Responder {
+    handle_embedded_file(path.as_str())
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(web_ui_index)
+            .service(web_ui_dist)
+    })
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
+}
+
