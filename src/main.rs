0c18dd4b399afe82ef483f045e4fb5b717ececbd
Yaşar İdikut <yasar8000@gmail.com>
2024-12-26 16:42:34 -0500

    parameterize server address

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,22 @@
-use actix_web::{App, HttpServer, Responder};
+use actix_web::{App, HttpServer};
 
 mod api;
 mod app;
 
+const DEFAULT_SERVER_ADDRESS_PORT: (&str, u16) = ("127.0.0.1", 8080);
+
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    println!("Hosting the frontend at http://{}:{}/", "0.0.0.0", 8080);
+    println!("Hosting the frontend at http://{}:{}/", DEFAULT_SERVER_ADDRESS_PORT.0, DEFAULT_SERVER_ADDRESS_PORT.1);
     HttpServer::new(|| {
         App::new()
             .service(app::web_ui_index)
-            .service(api::network)
-            .service(api::server_status)
+            .service(api::get_network)
+            .service(api::get_server_status)
             .service(app::web_ui_dist)
     })
-        .bind(("0.0.0.0", 8080))?
+        .bind(DEFAULT_SERVER_ADDRESS_PORT)?
         .run()
         .await
 }
