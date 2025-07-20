6d83d49f7994029819b8d84fe2f5d7be0b57679c
Yaşar İdikut <yasar8000@gmail.com>
2025-07-20 19:43:27 +0300

    update yaml store config

--- src/api.rs
+++ src/api.rs
@@ -1,9 +1,8 @@
 use actix_web::{get, web, HttpResponse, Responder};
-use sha2::{Digest, Sha256};
-use std::fs;
+use sha2::Digest;
 use std::hash::Hash;
 
-mod conf;
+pub(crate) mod conf;
 
 #[derive(serde::Deserialize)]
 struct SummaryBody {
@@ -12,25 +11,11 @@ struct SummaryBody {
 
 #[get("/api/summary")]
 async fn get_summary(params: web::Query<SummaryBody>) -> impl Responder {
-    let file_contents = fs::read_to_string(conf::DEFAULT_CONF_FILE).expect("Unable to open file");
-    let mut buf = [0u8; 64];
-    let network_digest: &str = base16ct::lower::encode_str(&Sha256::digest(file_contents.as_bytes()), &mut buf).expect("TODO: panic message");
-
     let mut resp_body = String::new();
     if (params.only_network_digest) {
-        let mut config_digest = conf::ConfigDigest {
-            network_digest: network_digest.to_string(),
-            status: conf::WireGuardStatus::UP.value(),
-            timestamp: 0,
-        };
-        config_digest.put_timestamp();
-        resp_body = serde_json::to_string(&config_digest).unwrap();
+        resp_body = serde_json::to_string(&conf::ConfigDigest::from(&conf::get_config())).unwrap();
     } else {
-        let mut config: conf::Config = serde_yml::from_str(&file_contents).unwrap();
-        config.network_digest = network_digest.to_string();
-        config.status = conf::WireGuardStatus::UP.value();  // TODO: check wg status
-        config.put_timestamp();
-        resp_body = serde_json::to_string(&config).unwrap();
+        resp_body = serde_json::to_string(&conf::get_config()).unwrap();
     }
 
     HttpResponse::Ok()
--- src/api/conf.rs
+++ src/api/conf.rs
@@ -1,5 +1,12 @@
 use serde::{Deserialize, Serialize};
+use serde_yml::Serializer;
+use sha2::{Digest, Sha256};
 use std::collections::HashMap;
+use std::fs;
+use std::fs::File;
+use std::hash::Hash;
+use std::io::Write;
+use std::ops::Deref;
 use std::time::SystemTime;
 
 pub(crate) const DEFAULT_CONF_FILE: &str = ".wg-rusteze/conf.yml";
@@ -20,9 +27,16 @@ impl WireGuardStatus {
     }
 }
 
-#[derive(Serialize, Deserialize, PartialEq, Debug)]
+#[derive(Serialize, Deserialize, PartialEq, Debug, Clone)]
+pub(crate) struct FileConfig {
+    pub(crate) agent: Agent,
+    pub(crate) network: Network,
+}
+
+#[derive(Serialize, Deserialize, PartialEq, Debug, Clone)]
 pub(crate) struct Config {
-    network: Network,
+    pub(crate) agent: Agent,
+    pub(crate) network: Network,
     #[serde(default)]
     pub(crate) network_digest: String,
     #[serde(default)]
@@ -41,7 +55,16 @@ impl Config {
     }
 }
 
-#[derive(Serialize, Deserialize, PartialEq, Debug)]
+impl From<&Config> for FileConfig {
+    fn from(config: &Config) -> Self {
+        FileConfig {
+            agent: config.agent.clone(),
+            network: config.network.clone(),
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, PartialEq, Debug, Clone)]
 pub(crate) struct ConfigDigest {
     #[serde(default)]
     pub(crate) network_digest: String,
@@ -50,6 +73,17 @@ pub(crate) struct ConfigDigest {
     #[serde(default)]
     pub(crate) timestamp: u64,
 }
+
+impl From<&Config> for ConfigDigest {
+    fn from(config: &Config) -> Self {
+        ConfigDigest {
+            network_digest: config.network_digest.clone(),
+            status: config.status.clone(),
+            timestamp: config.timestamp.clone(),
+        }
+    }
+}
+
 
 impl ConfigDigest {
     pub(crate) fn put_timestamp(&mut self) -> &mut Self {
@@ -61,7 +95,25 @@ impl ConfigDigest {
     }
 }
 
-#[derive(Serialize, Deserialize, PartialEq, Debug)]
+#[derive(Serialize, Deserialize, Clone, PartialEq, Debug)]
+pub(crate) struct Agent {
+    pub(crate) address: String,
+    pub(crate) web: AgentWeb,
+    pub(crate) vpn: AgentVpn,
+}
+
+#[derive(Serialize, Deserialize, PartialEq, Debug, Clone)]
+pub(crate) struct AgentWeb {
+    pub(crate) scheme: String,
+    pub(crate) port: u16,
+}
+
+#[derive(Serialize, Deserialize, PartialEq, Debug, Clone)]
+pub(crate) struct AgentVpn {
+    pub(crate) port: u16,
+}
+
+#[derive(Serialize, Deserialize, PartialEq, Debug, Clone)]
 struct Network {
     subnet: String,
     this_peer: String,
@@ -70,7 +122,7 @@ struct Network {
     defaults: Defaults,
 }
 
-#[derive(Serialize, Deserialize, PartialEq, Debug)]
+#[derive(Serialize, Deserialize, PartialEq, Debug, Clone)]
 struct Peer {
     name: String,
     address: String,
@@ -85,13 +137,13 @@ struct Peer {
     scripts: Scripts
 }
 
-#[derive(Serialize, Deserialize, PartialEq, Debug)]
+#[derive(Serialize, Deserialize, PartialEq, Debug, Clone)]
 struct EnabledValue { enabled: bool, value: String}
 
-#[derive(Serialize, Deserialize, PartialEq, Debug)]
+#[derive(Serialize, Deserialize, PartialEq, Debug, Clone)]
 struct Scripts { pre_up: EnabledValue, post_up: EnabledValue, pre_down: EnabledValue, post_down: EnabledValue }
 
-#[derive(Serialize, Deserialize, PartialEq, Debug)]
+#[derive(Serialize, Deserialize, PartialEq, Debug, Clone)]
 struct Connection {
     enabled: bool,
     pre_shared_key: String,
@@ -100,21 +152,48 @@ struct Connection {
     persistent_keepalive: EnabledValue,
 }
 
-#[derive(Serialize, Deserialize, PartialEq, Debug)]
+#[derive(Serialize, Deserialize, PartialEq, Debug, Clone)]
 struct Defaults {
     peer: DefaultPeer,
     connection: DefaultConnection,
 }
 
-#[derive(Serialize, Deserialize, PartialEq, Debug)]
+#[derive(Serialize, Deserialize, PartialEq, Debug, Clone)]
 struct DefaultPeer {
     dns: EnabledValue,
     mtu: EnabledValue,
     scripts: Scripts,
 }
 
-#[derive(Serialize, Deserialize, PartialEq, Debug)]
+#[derive(Serialize, Deserialize, PartialEq, Debug, Clone)]
 struct DefaultConnection {
     persistent_keepalive: EnabledValue,
 }
 
+pub(crate) fn get_config() -> Config {
+    let file_contents = fs::read_to_string(DEFAULT_CONF_FILE).expect("Unable to open file");
+    let mut config: Config = serde_yml::from_str(&file_contents).unwrap();
+
+    // Make sure agent fields get precedence over network fields
+    if config.network.peers.get(&config.network.this_peer).unwrap().endpoint != format!("{}:{}", config.agent.address, config.agent.vpn.port) {
+        config.network.peers.get_mut(&config.network.this_peer).unwrap().endpoint = format!("{}:{}", config.agent.address, config.agent.vpn.port);
+        set_config(&config);
+    }
+
+    let mut buf = [0u8; 64];
+    let network_digest: &str = base16ct::lower::encode_str(&Sha256::digest(file_contents.as_bytes()), &mut buf).expect("Unable to calculate network digest");
+    config.network_digest = network_digest.to_string();
+    config.status = WireGuardStatus::UP.value(); // TODO: replace
+    config.put_timestamp();
+
+    return config;
+}
+
+pub(crate) fn set_config(config: &Config) {
+    let file_config = FileConfig::from(config);
+    let config_str = serde_yml::to_string(&file_config).expect("Failed to serialize config");
+
+    let mut file = File::create(DEFAULT_CONF_FILE).expect("Failed to open config file");
+    file.write_all(config_str.as_bytes()).expect("Failed to write to config file");
+    log::info!("Updated config file")
+}
