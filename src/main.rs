91d44730a1d8dd84141e00a2196cee2c2fd02873
Yaşar İdikut <yasar8000@gmail.com>
2025-07-20 19:43:22 +0300

    use simple_logger

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,29 @@
 use actix_web::{App, HttpServer};
+use log::LevelFilter;
+use std::fs;
 
 mod api;
 mod app;
 
-const DEFAULT_SERVER_ADDRESS_PORT: (&str, u16) = ("127.0.0.1", 8080);
+use simple_logger::SimpleLogger;
 
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    println!("Hosting the frontend at http://{}:{}/", DEFAULT_SERVER_ADDRESS_PORT.0, DEFAULT_SERVER_ADDRESS_PORT.1);
+    SimpleLogger::new().with_level(LevelFilter::Info).init().unwrap();
+
+    let file_contents = fs::read_to_string(api::conf::DEFAULT_CONF_FILE).expect("Unable to open file");
+    let mut config: api::conf::Config = serde_yml::from_str(&file_contents).unwrap();
+
+    log::info!("Hosting the frontend at {}://{}:{}/", config.agent.web.scheme, config.agent.address, config.agent.web.port);
+
     HttpServer::new(|| {
         App::new()
             .service(app::web_ui_index)
             .service(api::get_summary)
             .service(app::web_ui_dist)
     })
-        .bind(DEFAULT_SERVER_ADDRESS_PORT)?
+        .bind((config.agent.address, config.agent.web.port))?
         .run()
         .await
 }
