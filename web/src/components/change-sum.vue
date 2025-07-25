16c32883af59bf28dfbeea04de311564efe1751b
Yaşar İdikut <yasar8000@gmail.com>
2025-07-25 09:45:25 +0300

    fix change-sum and show added/removed connections

--- web/src/components/change-sum.vue
+++ web/src/components/change-sum.vue
@@ -19,11 +19,17 @@
     </div>
     <div class="bg-gray-100 rounded-md overflow-scroll">
       <strong class="text-gray-600 justify-center rounded-md bg-gray-200 p-1">Old Configuration</strong>
-      <div class="p-1">{{ JSON.stringify(network, false, 2) }}</div>
+      <div class="p-1">{{
+          JSON.stringify({peers: pruned_network.peers, connections: pruned_network.connections}, false, 2)
+        }}
+      </div>
     </div>
     <div class="bg-green-100 rounded-md overflow-scroll">
       <strong class="text-gray-600 justify-center rounded-md bg-green-200 p-1">New Configuration</strong>
-      <div class="p-1">{{ JSON.stringify(new_network, false, 2) }}</div>
+      <div class="p-1">{{
+          JSON.stringify({peers: new_network.peers, connections: new_network.connections}, false, 2)
+        }}
+      </div>
     </div>
   </div>
 
@@ -60,23 +66,55 @@ export default {
     },
   },
   computed: {
-    new_network() {
-      let new_network = JSON.parse(JSON.stringify(this.network));
+    pruned_network() {
+      let pruned_network = {peers: {}, connections: {}};
+      pruned_network.peers[this.peerId] = this.network.peers[this.peerId];
 
-      if (!this.changeSum.changed_fields) {
-        return new_network;
-      }
-      for (const peer_field in this.changeSum.changed_fields.peers) {
-        if (peer_field === "scripts") {
-          for (const script_field in this.changeSum.changed_fields.peers.scripts[peer_field]) {
-            new_network.peers[this.peerId].scripts[script_field] = this.changeSum.changed_fields.peers.scripts[script_field];
-          }
-          continue;
+      for (let connection_id in this.network.connections) {
+        if (connection_id.includes(this.peerId)) {
+          pruned_network.connections[connection_id] = this.network.connections[connection_id];
         }
-        new_network.peers[this.peerId][peer_field] = this.changeSum.changed_fields.peers[peer_field];
+      }
+      return pruned_network;
+    },
+    new_network() {
+      let new_network = JSON.parse(JSON.stringify(this.pruned_network));
+
+      if (this.changeSum.changed_fields) {
+        // peer changes
+        for (const peer_id in this.changeSum.changed_fields.peers) {
+          for (const peer_field in this.changeSum.changed_fields.peers[peer_id]) {
+            if (peer_field === "scripts") {
+              for (const script_field in this.changeSum.changed_fields.peers[peer_id].scripts) {
+                new_network.peers[peer_id].scripts[script_field] = this.changeSum.changed_fields.peers[peer_id].scripts[script_field];
+              }
+              continue;
+            }
+            new_network.peers[peer_id][peer_field] = this.changeSum.changed_fields.peers[peer_id][peer_field];
+          }
+        }
+
+        // connection changes
+        for (const connection_id in this.changeSum.changed_fields.connections) {
+          for (const connection_field in this.changeSum.changed_fields.connections[connection_id]) {
+            new_network.connections[connection_id][connection_field] = this.changeSum.changed_fields.connections[connection_id][connection_field];
+          }
+        }
       }
 
-      return new_network
+      if (this.changeSum.added_connections) {
+        for (const added_connection_id in this.changeSum.added_connections) {
+          new_network.connections[added_connection_id] = this.changeSum.added_connections[added_connection_id];
+        }
+      }
+
+      if (this.changeSum.removed_connections) {
+        for (const removed_connection_id in this.changeSum.removed_connections) {
+          delete new_network.connections[removed_connection_id];
+        }
+      }
+
+      return new_network;
     },
   }
 }
