20285d998c31225cf45a779c99837bf0dd82aa95
Yaşar İdikut <yasar8000@gmail.com>
2025-08-03 14:47:50 +0300

    revamp cli

---
+++ agent/src/cli.rs
@@ -0,0 +1,87 @@
+use crate::macros::*;
+use clap::{ArgGroup, Args, Parser, Subcommand};
+use std::path::PathBuf;
+
+#[derive(Parser, Debug)]
+#[command(
+    version = full_version!(),
+    about = "A tool to manage the peer and network configuration of the \
+             WireGuard-based overlay network over the web console",
+)]
+pub(crate) struct Cli {
+    #[arg(short, long, help = "Increase verbosity level from Info to Debug")]
+    pub(crate) verbose: bool,
+    #[command(subcommand)]
+    pub(crate) command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+pub(crate) enum Commands {
+    #[command(about = "Initialize the wg-rusteze agent")]
+    Init {},
+    #[command(about = "Run some convenience functions to edit config")]
+    Config {
+        #[command(subcommand)]
+        commands: ConfigCommands,
+    },
+    #[command(about = "Configure and run the wg-rusteze agent")]
+    Agent {
+        #[arg(
+            long,
+            default_value = ".wg-rusteze/conf.yml",
+            value_name = "WG_RUSTEZE_CONFIG_FILE_PATH"
+        )]
+        wg_rusteze_config_file: PathBuf,
+        #[arg(
+            long,
+            default_value = "/opt/homebrew/etc/wireguard/",
+            value_name = "WIREGUARD_CONFIG_FOLDER_PATH"
+        )]
+        wireguard_config_folder: PathBuf,
+        #[arg(
+            long,
+            default_value = ".wg-rusteze/cert.pem",
+            value_name = "TLS_CERTIFICATE_FILE_PATH"
+        )]
+        tls_cert: PathBuf,
+        #[arg(
+            long,
+            default_value = ".wg-rusteze/key.pem",
+            value_name = "TLS_PRIVATE_KEY_FILE_PATH"
+        )]
+        tls_key: PathBuf,
+        #[command(subcommand)]
+        commands: AgentCommands,
+    },
+}
+
+#[derive(Subcommand, Debug)]
+pub(crate) enum AgentCommands {
+    #[command(about = "Runs the agent")]
+    Run(AgentRunOptions),
+}
+
+#[derive(Args, Debug)]
+#[command(group(
+    ArgGroup::new("mode")
+        .args(&["all", "only_web", "only_wireguard"])
+        .multiple(false)
+        .required(false)
+))]
+pub(crate) struct AgentRunOptions {
+    #[arg(
+        long,
+        help = "Start the wireguard server and run the web configuration portal"
+    )]
+    pub(crate) all: bool,
+    #[arg(long, help = "Run only the web configuration portal")]
+    pub(crate) only_web: bool,
+    #[arg(long, help = "Start only the wireguard server")]
+    pub(crate) only_wireguard: bool,
+}
+
+#[derive(Subcommand, Debug)]
+pub(crate) enum ConfigCommands {
+    #[command(about = "Reset the web password")]
+    ResetWebPassword,
+}
--- agent/src/main.rs
+++ agent/src/main.rs
@@ -1,3 +1,4 @@
+use crate::cli::{AgentCommands, ConfigCommands};
 use clap::Parser;
 use log::LevelFilter;
 use once_cell::sync::OnceCell;
@@ -6,87 +7,114 @@ use std::path::PathBuf;
 
 mod api;
 mod app;
+mod cli;
 mod conf;
 mod macros;
 mod server;
 mod wireguard;
-
-#[derive(Parser, Debug)]
-#[command(
-    version = full_version!(),
-    about = "Run the wg-rusteze network agent",
-    long_about = "Starts the wg-rusteze agent with the default/provided configuration file. \
-                  Use this tool to manage the peer and network configuration of the \
-                  WireGuard-based overlay network over the web console."
-)]
-struct Args {
-    #[arg(
-        long,
-        default_value = ".wg-rusteze/conf.yml",
-        value_name = "WG_RUSTEZE_CONFIG_FILE_PATH"
-    )]
-    wg_rusteze_config_file: PathBuf,
-    #[arg(
-        long,
-        default_value = "/opt/homebrew/etc/wireguard/",
-        value_name = "WIREGUARD_CONFIG_FOLDER_PATH"
-    )]
-    wireguard_config_folder: PathBuf,
-    #[arg(
-        long,
-        default_value = ".wg-rusteze/cert.pem",
-        value_name = "TLS_CERTIFICATE_FILE_PATH"
-    )]
-    tls_cert: PathBuf,
-    #[arg(
-        long,
-        default_value = ".wg-rusteze/key.pem",
-        value_name = "TLS_PRIVATE_KEY_FILE_PATH"
-    )]
-    tls_key: PathBuf,
-}
 
 pub static WG_RUSTEZE_CONFIG_FILE: OnceCell<PathBuf> = OnceCell::new();
 pub static WIREGUARD_CONFIG_FILE: OnceCell<PathBuf> = OnceCell::new();
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    // print version and start logger
+    let args = cli::Cli::parse();
     println!(full_version!());
+
+    // start logger
     SimpleLogger::new()
-        .with_level(LevelFilter::Info)
+        .with_level(if args.verbose {
+            LevelFilter::Debug
+        } else {
+            LevelFilter::Info
+        })
         .init()
         .unwrap_or_else(|e| {
             eprintln!("Logger init failed: {e}");
         });
 
-    // get the wg_rusteze config file path
-    let args = Args::parse();
-    WG_RUSTEZE_CONFIG_FILE
-        .set(args.wg_rusteze_config_file)
-        .expect("Failed to set WG_RUSTEZE_CONFIG_FILE");
-    log::info!(
-        "using the wg-rusteze config file at \"{}\"",
-        WG_RUSTEZE_CONFIG_FILE.get().unwrap().display()
-    );
+    match &args.command {
+        cli::Commands::Init {} => {
+            log::info!("Initializing wg-rusteze agent..."); // TODO: implement me
+            Ok(())
+        }
+        cli::Commands::Config { commands } => {
+            match commands {
+                ConfigCommands::ResetWebPassword => {
+                    log::info!("Resetting the web password..."); // TODO: implement me
+                }
+            }
+            Ok(())
+        }
+        cli::Commands::Agent {
+            wg_rusteze_config_file,
+            wireguard_config_folder,
+            tls_cert,
+            tls_key,
+            commands,
+        } => {
+            // get the wg_rusteze config file path
+            WG_RUSTEZE_CONFIG_FILE
+                .set(wg_rusteze_config_file.clone())
+                .expect("Failed to set WG_RUSTEZE_CONFIG_FILE");
+            log::info!(
+                "using the wg-rusteze config file at \"{}\"",
+                WG_RUSTEZE_CONFIG_FILE.get().unwrap().display()
+            );
 
-    // get the wireguard config file path
-    let config = conf::util::get_config();
-    let wireguard_config_filename = format!("{}.conf", config.network.identifier);
-    let wireguard_config_file_path = args.wireguard_config_folder.join(wireguard_config_filename);
-    WIREGUARD_CONFIG_FILE
-        .set(wireguard_config_file_path)
-        .expect("Failed to set WIREGUARD_CONFIG_FILE");
-    log::info!(
-        "using the wireguard config file at \"{}\"",
-        WIREGUARD_CONFIG_FILE.get().unwrap().display()
-    );
+            // get the wireguard config file path
+            let config = conf::util::get_config();
 
-    // start the tunnel
-    wireguard::cmd::start_tunnel(&config).unwrap_or_else(|e| {
-        log::error!("{e}");
-    });
+            let mut run_wireguard = true;
+            let mut run_web = true;
+            match commands {
+                AgentCommands::Run(opts) => {
+                    if opts.only_wireguard {
+                        run_web = false;
+                        log::info!(
+                            "--only-wireguard flag detected. Starting only the wireguard server..."
+                        )
+                    } else if opts.only_web {
+                        run_wireguard = false;
+                        log::info!(
+                            "--only-web flag detected. Running only the web configuration portal..."
+                        )
+                    } else if opts.all {
+                        log::info!(
+                            "--all flag detected. Starting the wireguard server and running the web configuration portal..."
+                        )
+                    } else {
+                        log::info!(
+                            "No run mode selected. Defaulting to --all (Starting the wireguard server and running the web configuration portal...)"
+                        );
+                    }
+                }
+            }
 
-    // start the HTTP server with TLS for frontend and API control
-    server::run_http_server(&config, &args.tls_cert, &args.tls_key).await
+            if run_wireguard {
+                WIREGUARD_CONFIG_FILE
+                    .set(
+                        wireguard_config_folder.join(format!("{}.conf", config.network.identifier)),
+                    )
+                    .expect("Failed to set WIREGUARD_CONFIG_FILE");
+                log::info!(
+                    "using the wireguard config file at \"{}\"",
+                    WIREGUARD_CONFIG_FILE.get().unwrap().display()
+                );
+
+                // start the tunnel
+                wireguard::cmd::start_tunnel(&config).unwrap_or_else(|e| {
+                    log::error!("{e}");
+                });
+            }
+
+            if run_web {
+                // start the HTTP server with TLS for frontend and API control
+                server::run_http_server(&config, tls_cert, tls_key)
+                    .await
+                    .expect("HTTP server failed to start");
+            }
+            Ok(())
+        }
+    }
 }
