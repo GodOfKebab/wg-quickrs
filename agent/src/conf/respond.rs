221a7e6a01463ec1658f79d1b7bb042d97cf1445
Yaşar İdikut <yasar8000@gmail.com>
2025-08-06 00:03:37 +0300

    improve error for responders

--- agent/src/conf/respond.rs
+++ agent/src/conf/respond.rs
@@ -1,14 +1,14 @@
+use crate::WG_RUSTEZE_CONFIG_FILE;
 use crate::conf::network;
 use crate::conf::timestamp;
-use crate::WG_RUSTEZE_CONFIG_FILE;
 use config_wasm::types::{Config, Lease};
 
 pub(crate) use crate::conf::util::get_config;
 use crate::conf::util::get_summary;
 use crate::wireguard::cmd::sync_conf;
-use actix_web::{web, HttpResponse};
+use actix_web::{HttpResponse, web};
 use chrono::Duration;
-use serde_json::{json, Value};
+use serde_json::{Value, json};
 use std::fs::OpenOptions;
 use std::io::{Read, Seek, SeekFrom, Write};
 use uuid::Uuid;
@@ -16,7 +16,9 @@ use uuid::Uuid;
 pub(crate) fn get_network_summary(query: web::Query<crate::api::SummaryBody>) -> HttpResponse {
     let summary = match get_summary() {
         Ok(summary) => summary,
-        Err(_) => { return HttpResponse::InternalServerError().body("Unable to get config"); }
+        Err(_) => {
+            return HttpResponse::InternalServerError().body("Unable to get config");
+        }
     };
     let response_data = if query.only_digest {
         json!(config_wasm::types::SummaryDigest::from(&summary))
@@ -241,40 +243,52 @@ fn apply_changes(network_config: &mut Value, section_name: &str, changed_fields:
 
 pub(crate) fn get_network_lease_id_address() -> HttpResponse {
     // Open the config file for reading and writing
-    let mut config_file_reader = OpenOptions::new()
+    let mut config_file_reader = match OpenOptions::new()
         .read(true)
         .write(true)
-        .open(
-            WG_RUSTEZE_CONFIG_FILE
-                .get()
-                .expect("WG_RUSTEZE_CONFIG_FILE not set"),
-        )
-        .expect("Failed to open config file");
+        .open(WG_RUSTEZE_CONFIG_FILE.get().unwrap())
+    {
+        Ok(file) => file,
+        Err(_) => {
+            return HttpResponse::InternalServerError()
+                .body("Failed to open config file".to_string());
+        }
+    };
 
     // Read the existing contents
-    let mut config_str = String::new();
-    config_file_reader
-        .read_to_string(&mut config_str)
-        .expect("Failed to read config file");
-    let mut config_file: Config = serde_yml::from_str(&config_str).unwrap();
+    let mut file_contents = String::new();
+    match config_file_reader.read_to_string(&mut file_contents) {
+        Ok(_) => {}
+        Err(_) => {
+            return HttpResponse::InternalServerError()
+                .body("Failed to read config file".to_string());
+        }
+    }
+    let mut config: Config = match serde_yml::from_str(&file_contents) {
+        Ok(c) => c,
+        Err(_) => {
+            return HttpResponse::InternalServerError()
+                .body("Failed to read config file".to_string());
+        }
+    };
 
     let mut reserved_addresses = Vec::<String>::new();
-    for peer in config_file.network.peers.values() {
+    for peer in config.network.peers.values() {
         reserved_addresses.push(peer.address.clone());
     }
-    config_file.network.leases.retain(|lease| {
+    config.network.leases.retain(|lease| {
         timestamp::get_duration_since_formatted(lease.valid_until.clone()) > Duration::zero()
     });
 
-    for lease in config_file.network.leases.clone() {
+    for lease in config.network.leases.clone() {
         reserved_addresses.push(lease.address.clone());
     }
     let next_address =
-        match network::get_next_available_address(&config_file.network.subnet, &reserved_addresses)
-        {
+        match network::get_next_available_address(&config.network.subnet, &reserved_addresses) {
             Some(next_address) => next_address,
             None => {
-                return HttpResponse::InternalServerError().into();
+                return HttpResponse::InternalServerError()
+                    .body("Failed to get next available address".to_string());
             }
         };
 
@@ -284,19 +298,36 @@ pub(crate) fn get_network_lease_id_address() -> HttpResponse {
         valid_until: timestamp::get_future_timestamp_formatted(Duration::minutes(10)),
     };
     log::info!("leased address {} until {}", body.address, body.valid_until);
-    config_file.network.leases.push(body.clone());
-    config_file.network.updated_at = timestamp::get_now_timestamp_formatted();
-    let config_str = serde_yml::to_string(&config_file).expect("Failed to serialize config");
-    // Move back to the beginning and truncate before writing
-    config_file_reader
-        .set_len(0)
-        .expect("Failed to truncate config file");
-    config_file_reader
-        .seek(SeekFrom::Start(0))
-        .expect("Failed to seek to start");
-    config_file_reader
-        .write_all(config_str.as_bytes())
-        .expect("Failed to write to config file");
+    config.network.leases.push(body.clone());
+    config.network.updated_at = timestamp::get_now_timestamp_formatted();
+    file_contents = match serde_yml::to_string(&config) {
+        Ok(c) => c,
+        Err(_) => {
+            return HttpResponse::InternalServerError()
+                .body("Failed to serialize config".to_string());
+        }
+    };
 
+    // Move back to the beginning and truncate before writing
+    match config_file_reader.set_len(0) {
+        Ok(_) => {}
+        Err(_) => {
+            return HttpResponse::InternalServerError()
+                .body("Failed to truncate config file".to_string());
+        }
+    }
+    match config_file_reader.seek(SeekFrom::Start(0)) {
+        Ok(_) => {}
+        Err(_) => {
+            return HttpResponse::InternalServerError().body("Failed to seek to start".to_string());
+        }
+    }
+    match config_file_reader.write_all(file_contents.as_bytes()) {
+        Ok(_) => {}
+        Err(_) => {
+            return HttpResponse::InternalServerError()
+                .body("Failed to write to config file".to_string());
+        }
+    }
     HttpResponse::Ok().json(json!(body))
 }
