7d4b9d5a7280be5a946da61330c43d80dac524ac
Yaşar İdikut <yasar8000@gmail.com>
2025-09-03 21:23:35 -0400

    make tls generation scripts work on MacOS/Linux

--- cert/make-tls-certs.sh
+++ cert/make-tls-certs.sh
@@ -1,55 +1,69 @@
-# If /app/certs/root does NOT exist, create it
-if [ ! -d /app/certs/root ]
+#!/usr/bin/env bash
+set -euo pipefail
+
+# Detect OpenSSL flavor and set correct flag
+OPENSSL_REQ_NOENC_FLAG="-noenc"
+if openssl version 2>/dev/null | grep -qi "LibreSSL"; then
+    # macOS (LibreSSL doesn't support -noenc)
+    OPENSSL_REQ_NOENC_FLAG="-nodes"
+fi
+
+# If certs/root does NOT exist, create it
+if [ ! -d certs/root ]
 then
-    mkdir /app/certs/root
+    mkdir -p certs/root
 fi
 
 # Create a Root Certificate and self-sign it
-# If /app/certs/rootCA.key does NOT exist, create it
-if [ ! -f /app/certs/root/rootCA.key ]
+# If certs/rootCA.key does NOT exist, create it
+if [ ! -f certs/root/rootCA.key ]
 then
     # Create the root key
-    openssl genrsa -out /app/certs/root/rootCA.key 4096
+    openssl genrsa -out certs/root/rootCA.key 4096
 fi
 
-# If /app/certs/rootCA.crt does NOT exist, create it
-if [ ! -f /app/certs/root/rootCA.crt ]
+# If certs/rootCA.crt does NOT exist, create it
+if [ ! -f certs/root/rootCA.crt ]
 then
     # Generate the Root Certificate.
-    openssl req -x509 -sha256 -new -noenc -key /app/certs/root/rootCA.key -days 800 -out /app/certs/root/rootCA.crt  -subj "/C=$COUNTRY/ST=$STATE/L=$LOCALITY/O=$ORGANIZATION CA/OU=$ORGANIZATIONAL_UNIT/CN=$ROOT_CN"
+    openssl req -x509 -sha256 -new $OPENSSL_REQ_NOENC_FLAG \
+        -key certs/root/rootCA.key \
+        -out certs/root/rootCA.crt \
+        -subj "/C=$COUNTRY/ST=$STATE/L=$LOCALITY/O=$ORGANIZATION CA/OU=$ORGANIZATIONAL_UNIT/CN=$ROOT_CN" \
+        -days 800
 fi
 
-# If /app/certs/servers does NOT exist, create it
-if [ ! -d /app/certs/servers ]
+# If certs/servers does NOT exist, create it
+if [ ! -d certs/servers ]
 then
-    mkdir /app/certs/servers
+    mkdir -p certs/servers
 fi
 
 # Create certificates for servers
 for server in "$@"
 do
-    # If /app/certs/servers/$server does NOT exist, create it
-    if [ ! -d "/app/certs/servers/$server" ]
+    # If certs/servers/$server does NOT exist, create it
+    if [ ! -d "certs/servers/$server" ]
     then
-        mkdir "/app/certs/servers/$server"
+        mkdir "certs/servers/$server"
     fi
 
     # Create the certificate's key if it doesn't exist
-    if [ ! -f "/app/certs/servers/$server/key.pem" ]
+    if [ ! -f "certs/servers/$server/key.pem" ]
     then
         openssl genpkey -algorithm RSA \
-            -out "/app/certs/servers/$server/key.pem" \
+            -out "certs/servers/$server/key.pem" \
             -pkeyopt rsa_keygen_bits:4096
     fi
 
     # Generate the Certificate Signing Request (CSR)
     openssl req -new \
-        -key "/app/certs/servers/$server/key.pem" \
-        -out "/app/certs/servers/$server.csr" \
+        -key "certs/servers/$server/key.pem" \
+        -out "certs/servers/$server.csr" \
         -subj "/C=$COUNTRY/ST=$STATE/L=$LOCALITY/O=$ORGANIZATION certificate/OU=$ORGANIZATIONAL_UNIT/CN=$server"
 
     # Configure extensions so browsers don't yell
-    cat > "/app/certs/servers/$server.ext" << EOF
+    cat > "certs/servers/$server.ext" << EOF
 authorityKeyIdentifier=keyid,issuer
 basicConstraints=CA:FALSE
 keyUsage = digitalSignature, nonRepudiation, keyEncipherment, dataEncipherment
@@ -60,19 +74,19 @@ EOF
 
     # Finally create the certificate for the server and sign it using CA
     openssl x509 -req \
-        -in "/app/certs/servers/$server.csr" \
-        -CA /app/certs/root/rootCA.crt \
-        -CAkey /app/certs/root/rootCA.key \
+        -in "certs/servers/$server.csr" \
+        -CA certs/root/rootCA.crt \
+        -CAkey certs/root/rootCA.key \
         -CAcreateserial \
-        -out "/app/certs/servers/$server/cert.pem" \
+        -out "certs/servers/$server/cert.pem" \
         -days 825 -sha256 \
-        -extfile "/app/certs/servers/$server.ext"
+        -extfile "certs/servers/$server.ext"
 
     # Remove unnecessary files: Certificate Signing Request (CSR).
-    rm "/app/certs/servers/$server.csr" "/app/certs/servers/$server.ext"
+    rm "certs/servers/$server.csr" "certs/servers/$server.ext" certs/root/rootCA.srl
 
     # Verify the newly created certificate
-    echo "Created a certificate and a key for '$server' at /app/certs/servers/$server/cert.pem and /app/certs/servers/$server/key.pem"
-    openssl x509 -in "/app/certs/servers/$server/cert.pem" -text -noout
+    echo "Created a certificate and a key for '$server' at certs/servers/$server/cert.pem and certs/servers/$server/key.pem"
+    openssl x509 -in "certs/servers/$server/cert.pem" -text -noout
 done
 
