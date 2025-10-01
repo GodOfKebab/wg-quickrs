e323da4860eb9936b47c3c0e0d31378a803caf6c
Yaşar İdikut <yasar8000@gmail.com>
2025-10-01 10:38:43 -0400

    move undo.svg inline

--- src/web/src/components/ui/undo-button.vue
+++ src/web/src/components/ui/undo-button.vue
@@ -5,7 +5,12 @@
         class="align-middle p-0.5 rounded bg-gray-100 hover:bg-gray-500 hover:text-white transition"
         title="Undo Changes"
         @click="$emit('click');">
-      <img :class="imageClasses" alt="Undo" src="/icons/flowbite/undo.svg"/>
+      <!-- "undo" @ https://flowbite.com/icons/ -->
+      <svg :class="imageClasses" class="fill-none" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+           width="24" height="24" viewBox="0 0 24 24">
+        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+              d="M3 9h13a5 5 0 0 1 0 10H7M3 9l4-4M3 9l4 4"/>
+      </svg>
     </button>
   </div>
 </template>
