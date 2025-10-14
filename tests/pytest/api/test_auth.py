a74bf4292f177408e70ef6aca43cf7c19298e5df
Yaşar İdikut <yasar8000@gmail.com>
2025-10-14 19:02:06 -0400

    fix a typo

--- tests/pytest/api/test_auth.py
+++ tests/pytest/api/test_auth.py
@@ -10,13 +10,13 @@ def test_api_token(setup_wg_quickrs_agent):
     # Try getting a token with the correct password
     response = make_post_request_with_retries(f"{base_url}/api/token",
                                               json={ "client_id": "pytest", "password": "test" },
-                                              verify=pytest_folder / ".wg-quickrs-pytest/certs/root/rootCA.crt")
+                                              verify=wg_quickrs_config_folder / "certs/root/rootCA.crt")
     assert response.status_code == 200
     assert response.text.startswith("ey")
 
     # Try getting a token with the wrong password
     response = make_post_request_with_retries(f"{base_url}/api/token",
                                               json={ "client_id": "pytest", "password": "..." },
-                                              verify=pytest_folder / ".wg-quickrs-pytest/certs/root/rootCA.crt")
+                                              verify=wg_quickrs_config_folder / "certs/root/rootCA.crt")
     assert response.status_code == 401
     assert not response.text.startswith("ey")
